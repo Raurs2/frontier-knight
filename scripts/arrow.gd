739fd0fa96d6f5e71ffd82248776870957ccbd2c
Raul Alexandre Gonzalez Augusto <166350834+Raurs2@users.noreply.github.com>
2025-03-19 14:00:22 -0300

    Update arrow.gd

--- scripts/arrow.gd
+++ scripts/arrow.gd
@@ -15,8 +15,6 @@ func _physics_process(delta: float) -> void:
 	if travelled_distance > RANGE:
 		queue_free()
 	
-
-
 func _on_body_entered(body: Node2D) -> void:
 	if body.has_method("take_damage"):
 		body.take_damage('bow')
