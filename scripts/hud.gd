f711a64a313ce66f3ead1d4e140a7c4e8009dfdf
Raul Alexandre Gonzalez Augusto <166350834+Raurs2@users.noreply.github.com>
2025-03-19 14:20:33 -0300

    Update hud.gd

--- scripts/hud.gd
+++ scripts/hud.gd
@@ -7,16 +7,10 @@ class_name Hud
 @onready var time_label: Label = $CenterContainer/HBoxContainer/HBoxContainer2/TimeLabel
 @onready var money_label: Label = $CenterContainer/HBoxContainer/HBoxContainer3/MoneyLabel
 
-
 func _process(_delta: float) -> void:
-	show_stats()
-
-func show_stats():
 	time_label.text = "%d:%02d" % [floor(end_timer.time_left / 60), int(end_timer.time_left) % 60]
 	enemies_label.text = str(SaveManager.stats.kill_count)
 	money_label.text = str(SaveManager.stats.coins_earned)
-
-
 
 func _on_end_timer_timeout() -> void:
 	level_end_score.visible = true
