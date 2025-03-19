b9b56a52fb04e98f10d55daecae98b23d6bc7480
Raul Alexandre Gonzalez Augusto <166350834+Raurs2@users.noreply.github.com>
2025-03-19 15:16:51 -0300

    Update prologue_2.gd

--- scripts/prologue_2.gd
+++ scripts/prologue_2.gd
@@ -1,12 +1,14 @@
 extends Control
 
-@onready var background: TextureRect = $background
-@onready var dialog_box: DialogBox = $DialogBox
-@onready var bgm: Bgm = $Bgm
 const HEROINE = preload("res://assets/prologue/heroine.png")
 const BLOOD = preload("res://assets/prologue/blood.png")
 const DIRT = preload("res://assets/prologue/dirt.png")
 const STARS = preload("res://assets/prologue/stars.png")
+
+@onready var background: TextureRect = $background
+@onready var dialog_box: DialogBox = $DialogBox
+@onready var bgm: Bgm = $Bgm
+
 func _ready() -> void:
 	get_tree().paused = false
 	dialog_box.is_dialog_started = true
@@ -34,7 +36,6 @@ func change_background(new_texture: Texture2D) -> void:
 		return  
 
 	var tween = create_tween().set_trans(Tween.TRANS_LINEAR).set_ease(Tween.EASE_IN_OUT)
-
 	tween.tween_property(background, "modulate", Color(1, 1, 1, 0), 0.1)
 	await tween.finished  # Wait until the fade-out is complete
 
